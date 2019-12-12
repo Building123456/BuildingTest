5e00029c185827befec3dc4af65031ef8d659308
abdullah96f <abdullah.agla@gmail.com>
2019-12-12 13:13:34 +0300

    00

---
+++ map/app.js
@@ -0,0 +1,11 @@
+
+var building = document.querySelector('canvas')
+var ctx = building.getContext('2d');
+var canvasContainer = document.querySelector('canvasContainer');
+
+
+building.height = canvasContainer.height;
+building.width = canvasContainer.width;
+
+
+
