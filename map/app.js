ded4b7a475f63f0f160ad92729829c4646962652
abdullah96f <abdullah.agla@gmail.com>
2019-12-17 13:32:47 +0300

    version 0.1

--- map/app.js
+++ map/app.js
@@ -1,14 +1,22 @@
 
-var building = document.querySelector('canvas')
-var ctx = building.getContext('2d');
-var canvasContainer = document.querySelector('canvasContainer');
+// var building = document.querySelector('canvas')
+// var ctx = building.getContext('2d');
+// var canvasContainer = document.querySelector('canvasContainer');
 
 
-building.height = canvasContainer.height;
-building.width = canvasContainer.width;
+// building.height = canvasContainer.height;
+// building.width = canvasContainer.width;
 
+$(".b2").click(function(){
+    $("#b2").attr("background-color" , "blue");
+    console.log('ddd')
+})
 
+ $("#find").click(function(){
+     window.open('https://www.w3schools.com/jsref/met_win_open.asp');
+ })
 
-//t4rt 4r t
+//t4rt 4r 
+
 
 //ijih
