3d848c8e1cffde159c2ed81064d8b3f4d779bdf4
Ahmedokokok <55056841+Ahmedokokok@users.noreply.github.com>
2019-12-17 13:38:29 +0300

    hey

---
+++ map/Main Building/app.js
@@ -0,0 +1,13 @@
+
+var building = document.querySelector('canvas')
+var ctx = building.getContext('2d');
+var canvasContainer = document.querySelector('canvasContainer');
+
+
+building.height = canvasContainer.height;
+building.width = canvasContainer.width;
+
+
+//t4rt 4r t
+
+//ijih
