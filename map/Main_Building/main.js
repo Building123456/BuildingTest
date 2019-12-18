e0a407e43ad141f319e010e04396e7bf8b519514
abdullah96f <abdullah.agla@gmail.com>
2019-12-18 14:16:08 +0300

    005

--- map/Main_Building/main.js
+++ map/Main_Building/main.js
@@ -1,13 +1,56 @@
-var modal = $(".modal");
-var closeButton = $(".close-button");
 
+$(document).ready(function(){
+
+
+let res={
+    "case": true,
+    "caseType": "network",
+    "toDep":"IT",
+    "message" :"مكيف خربان هيلب بليز",
+    "building": "0",
+    "floor": "0",
+     "id": "003",
+}
+
+    
+var $modal = $(".modal");
+var $modal_content = $(".modal-content");
+var $closeButton = $(".close-button");
+var clearText= false;
+var $message = $('#message');
 
 $("canvas").on('click',function(){
     ShowProblemMessage( $(this).attr('id') );
     
 });
 
+$closeButton.on('click',function(){
+    $modal.toggleClass('show-modal');
+    $modal_content.toggle("fast" ,ShowModal());
+})
+
+function ShowModal(){
+        $modal_content.addClass('show') 
+}
 
 function ShowProblemMessage(id){
-    modal.toggleClass(".show-modal")
+    $modal.toggleClass('show-modal')
+         temp = LoadData(id);
+         $message.append(temp.message);
+    $modal_content.toggle("fast" ,ShowModal());
 }
+
+
+function LoadData(id){
+    $message.empty();
+    return res;
+}
+
+
+
+
+
+})
+
+
+
